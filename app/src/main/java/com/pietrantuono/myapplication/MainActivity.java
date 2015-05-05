9350a3aa12a0a307e1808253455c2cc0e99d027c
Maurizio <maurizio.pietrantuono@gmail.com>
2015-05-05 07:53:32 +0200

    First commit

---
+++ app/src/main/java/com/pietrantuono/myapplication/MainActivity.java
@@ -0,0 +1,58 @@
+package com.pietrantuono.myapplication;
+
+import com.google.android.glass.media.Sounds;
+import com.google.android.glass.widget.CardBuilder;
+import com.google.android.glass.widget.CardScrollAdapter;
+import com.google.android.glass.widget.CardScrollView;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
+import android.media.AudioManager;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+
+import net.majorkernelpanic.streaming.SessionBuilder;
+import net.majorkernelpanic.streaming.gl.SurfaceView;
+import net.majorkernelpanic.streaming.rtsp.RtspServer;
+
+
+public class MainActivity extends Activity {
+    private final static String TAG = "MainActivity";
+
+    private SurfaceView mSurfaceView;
+
+    @Override
+    protected void onCreate(Bundle bundle) {
+        super.onCreate(bundle);
+        setContentView(R.layout.activity_main);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
+        mSurfaceView = (SurfaceView) findViewById(R.id.surface);
+
+        // Sets the port of the RTSP server to 1234
+        SharedPreferences.Editor editor = PreferenceManager.getDefaultSharedPreferences(this).edit();
+        editor.putString(RtspServer.KEY_PORT, String.valueOf(1234));
+        editor.commit();
+
+        // Configures the SessionBuilder
+        SessionBuilder.getInstance()
+                .setSurfaceView(mSurfaceView)
+                .setPreviewOrientation(90)
+                .setContext(getApplicationContext())
+                .setAudioEncoder(SessionBuilder.AUDIO_NONE)
+                .setVideoEncoder(SessionBuilder.VIDEO_H264);
+
+        // Starts the RTSP server
+        this.startService(new Intent(this, RtspServer.class));
+    }
+
+
+
+
+}
